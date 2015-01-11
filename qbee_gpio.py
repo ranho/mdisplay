622729c6111b831435a27b09c1fd1295bb207f6f
ranho <tigs2001inspace@gmail.com>
2015-01-11 17:16:46 +0000

    Update qbee_gpio.py

--- qbee_gpio.py
+++ qbee_gpio.py
@@ -28,14 +28,17 @@ import select
 import logging
 
 # GPIO pin configuration (BCM).
-AMP_POWER_PIN = 4
-LCD_POWER_PIN = 17
-LCD_RS_PIN = 27
-LCD_E_PIN = 18
-LCD_D4_PIN = 25
-LCD_D5_PIN = 24
-LCD_D6_PIN = 23
-LCD_D7_PIN = 22
+LCD_POWER_PIN = 02
+LCD_RS_PIN = 26
+LCD_E_PIN = 24
+LCD_D4_PIN = 22
+LCD_D5_PIN = 18
+LCD_D6_PIN = 16
+LCD_D7_PIN = 12
+
+#RUNEAUDIO MODIFICATION
+do_something( json_encode($status));
+{"volume":"75","repeat":"0","random":"0","single":"0","consume":"0","playlist":"31","playlistlength":"4902","mixrampdb":"0.000000","state":"play","song":"2153","songid":"2154","time":"286","elapsed":"197","bitrate":"912","audio":"44100:16:2","nextsong":"2154","nextsongid":"2155","OK":null,"song_percent":69,"audio_sample_rate":"44.1","audio_sample_depth":"16","audio_channels":"Stereo","changed":"playlist","currentartist":"John Williams","currentsong":"Luke and Leia","currentalbum":"Star Wars: Episode VI - Return of the Jedi","fileext":"flac","radioname":null}
 
 # Variables allowing to fetch information from sound output use.
 SOUND_PROCESSES = {'AirPlay': 'shairport',
